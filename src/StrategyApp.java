73beafed462bed5aa806eb4c797236a055bd1b73
oksana <oksanaov@gmail.com>
2020-03-20 00:20:06 +0200

    selection sorting strategy added

--- src/StrategyApp.java
+++ src/StrategyApp.java
@@ -7,6 +7,10 @@ public class StrategyApp {
 
         int[] arr = {32,1,6,-6, 43, 234, 54, 4};
         client.executeStrategy(arr);
+
+        client.setStrategy(new SelectionSort());
+        int[] arr1 = {6, 3, 28, 0, 13, 89, 4};
+        client.executeStrategy(arr1);
     }
 }
 
@@ -37,6 +41,30 @@ class BubbleSort implements Sorting{
                     arr[j] = temp;
                 }
             }
+        }
+        System.out.println("after: \t"+ Arrays.toString(arr));
+    }
+}
+
+class SelectionSort implements Sorting{
+
+    public void sort(int[] arr) {
+        System.out.println("SelectionSort");
+        System.out.println("before: \t"+ Arrays.toString(arr));
+        int min;
+        int minIndex;
+        for(int i = 0; i<arr.length;i++){
+            min = arr[i];
+            minIndex = i;
+            for (int j = i+1; j<arr.length; j++){
+                if (arr[j] < min){
+                    minIndex = j;
+                    min = arr[j];
+                }
+            }
+            int temp = arr[i];
+            arr[i] = min;
+            arr[minIndex] = temp;
         }
         System.out.println("after: \t"+ Arrays.toString(arr));
     }
